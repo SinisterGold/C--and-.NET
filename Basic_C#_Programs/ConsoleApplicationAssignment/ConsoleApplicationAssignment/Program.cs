7f56d2b3e3955fb882144a50c6c07abb645ec6f0
SinisterGold <128642241+SinisterGold@users.noreply.github.com>
2025-03-09 18:01:14 -0600

    Assignment Submission
    Uploaded Anonymous Income Comparison Program and Console Application Assignment

---
+++ Basic_C#_Programs/ConsoleApplicationAssignment/ConsoleApplicationAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Takes an input from the user, multiplies it by 50, then prints the result to the console.
+            Console.WriteLine("Type your favorite whole number we will then multiply it by 50");
+            string Num = Console.ReadLine();
+            int favNum = Convert.ToInt32(Num);
+            int productNum = favNum * 50;
+            Console.WriteLine("Your number Multiplied by 50 is:" + productNum.ToString());
+
+            //Takes an input from the user, adds 25 to it, then prints the result to the console.
+            Console.WriteLine("Type your favorite whole number we will then add 25 to it");
+            string inputNum = Console.ReadLine();
+            int intNum = Convert.ToInt32(inputNum);
+            int sumNum = intNum + 25;
+            Console.WriteLine("Your number added to 25 is:" + sumNum.ToString());
+
+            //Takes an input from the user, divides it by 12.5, then prints the result to the console.
+            Console.WriteLine("Type your favorite whole number we will then divide it by 12.5");
+            string typedNum = Console.ReadLine();
+            double numerator = Convert.ToDouble(typedNum);
+            double result = numerator / 12.5;
+            Console.WriteLine("Your number divided by 12.5 is:" + result.ToString());
+
+
+            //Takes an input from the user, checks if it is greater than 50, then prints the true/false result to the console.
+            Console.WriteLine("Input a number a whole number between 1 and 100 and we will see if it greater then my number");
+            string guessed = Console.ReadLine();
+            int guessedNum = Convert.ToInt32(guessed);
+            bool tOrf = guessedNum > 50;
+            Console.WriteLine("Is the number you guessed greater then my number:" + tOrf.ToString());
+
+
+
+            //Takes an input from the user, divides it by 7, then prints the remainder to the console
+            Console.WriteLine("Input a number and we will see if its divisable by 7. If it is the remainder will be 0");
+            string argNum = Console.ReadLine();
+            int intArgNum = Convert.ToInt32(argNum);
+            int remainder = intArgNum % 7;
+            Console.WriteLine("Your Number Divided by 7 has a remainder of:" + remainder.ToString());
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/MathandComparisonOperatorsAssignment/MathandComparisonOperatorsAssignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathandComparisonOperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program Title
+            Console.WriteLine("Anonymous Income Comparison Program\n");
+
+            //Person 1
+
+            //P1 Rate
+            Console.WriteLine("Person 1\n");
+            Console.WriteLine("What is your hourly rate?");
+            string p1Rate = Console.ReadLine();
+            int intp1Rate = Convert.ToInt32(p1Rate);
+
+            //P1 Hours
+            Console.WriteLine("\nHow many hours do you work a week?");
+            string p1Hours = Console.ReadLine();
+            int intp1Hours = Convert.ToInt32(p1Hours);
+
+
+            //Person 2
+
+            //P2 Rate
+            Console.WriteLine("\nPerson 2\n");
+            Console.WriteLine("What is your hourly rate?");
+            string p2Rate = Console.ReadLine();
+            int intp2Rate = Convert.ToInt32(p2Rate);
+
+            //P2 Hours
+            Console.WriteLine("\nHow many hours do you work a week?");
+            string p2Hours = Console.ReadLine();
+            int intp2Hours = Convert.ToInt32(p2Hours);
+
+
+
+            //Annual Salary of Person 1
+            int p1annualSalary = intp1Rate * intp1Hours * 52; //52 weeks in a year
+            Console.WriteLine("\nPerson 1 Annual Salary is: " +"$" + p1annualSalary.ToString());
+
+            //Annual Salary of Person 2
+            int p2annualSalary = intp2Rate * intp2Hours * 52; //52 weeks in a year
+            Console.WriteLine("\nPerson 1 Annual Salary is: " + "$" + p2annualSalary.ToString());
+
+
+            //Person 1 and 2 Comparison
+            Console.WriteLine("\nPerson 1 makes more money than Person 2?");
+            bool trueOrfalse = p1annualSalary > p2annualSalary;
+            Console.WriteLine(trueOrfalse);
+            Console.ReadLine();
+        }
+    }
+}
